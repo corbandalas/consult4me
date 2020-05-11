557e283ec77cf02a7342edf2797fbecf9dc8b1a0
corbandalas <me@corbandalas.com>
2020-05-11 10:18:33 +0300

    Initial commit

---
+++ src/main/java/studio/secretingredients/consult4me/Consult4meApplication.java
@@ -0,0 +1,13 @@
+package studio.secretingredients.consult4me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consult4meApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consult4meApplication.class, args);
+	}
+
+}
---
+++ src/test/java/studio/secretingredients/consult4me/Consult4meApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.secretingredients.consult4me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consult4meApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
